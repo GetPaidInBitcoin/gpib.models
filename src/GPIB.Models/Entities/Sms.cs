a10da6daa93c1ae843154660ba4f9e86298804c1
Lucas Cullen <lucas@bitcoinbrisbane.com.au>
2019-03-12 08:13:12 +1000

    Add entities

---
+++ src/GPIB.Models/Entities/Sms.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace GPIB.Models.Entities
+{
+    public class Sms : BaseUserEntity, IUserEntity, ISms
+    {
+        public String Message { get; set; }
+
+        public DateTime Sent { get; set; }
+
+        public DateTime Expires { get; set; }
+    }
+}
---
+++ src/GPIB.Models/ISms.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace GPIB.Models
+{
+    public interface ISms
+    {
+        int ID { get; set; }
+
+        Guid UserID { get; set; }
+
+        DateTime Expires { get; set; }
+
+        string Message { get; set; }
+
+        DateTime Sent { get; set; }
+    }
+}
