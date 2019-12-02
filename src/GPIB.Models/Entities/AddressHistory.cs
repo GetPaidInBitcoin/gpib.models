636a2b30d9fd66655a5b7d4cea1102aca0f32276
Lucas Cullen <lucas@bitcoinbrisbane.com.au>
2019-12-02 18:39:10 +1000

    Update models

--- src/GPIB.Models/Entities/AddressHistory.cs
+++ src/GPIB.Models/Entities/AddressHistory.cs
@@ -1,7 +1,15 @@
+using System;
+
 namespace GPIB.Models
 {
-    public class AddressHistory
+    public class AddressHistory : BaseUserEntity
     {
+        public Int32 AddressID { get; set; }
         
+        public String IPAddress { get; set; }
+        
+        public DateTime Created { get; set; }
+        
+        public String Message { get; set; }
     }
 }
