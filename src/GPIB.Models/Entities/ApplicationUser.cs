8b2457d6d6f2c7fc152d0aa0e830fe07d1589151
Lucas Cullen <lucas@bitcoinbrisbane.com.au>
2020-01-05 11:35:17 +1000

    Update

--- src/GPIB.Models/Entities/ApplicationUser.cs
+++ src/GPIB.Models/Entities/ApplicationUser.cs
@@ -4,14 +4,12 @@ namespace GPIB.Models
 {
     public class ApplicationUser : IUser
     {
-        public String ID { get; set; }
+        public String Id { get; set; }
         
         public String Email { get; set; }
         
         public Boolean EmailConfirmed { get; set; }
         
         public String PasswordHash { get; set; }
-        
-        public Boolean AccountLocked { get; set; }
     }
 }
