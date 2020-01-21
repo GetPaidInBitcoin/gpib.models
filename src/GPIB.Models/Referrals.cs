6f69aeda75bd2f494d0ab1cee9fa0194bfb4bfb1
Justin Taylor <s.justin.taylor@gmail.com>
2020-01-21 15:08:23 +1000

    added referrals model

---
+++ src/GPIB.Models/Referrals.cs
@@ -0,0 +1,25 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GPIB.Models
+{
+    public class Referrals
+    {
+        [Key]
+        [Required]
+        int ID { get; set; }
+
+        [Required]
+        Guid UserID { get; set; }
+
+        [Required]
+        decimal Amount { get; set; }
+
+        [Required]
+        String Coin { get; set; }
+
+        [Required]
+        decimal Rate { get; set; }
+    }
+}
