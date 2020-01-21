be4d305fe51a2db42e4e593aa2ef30c65586363c
Justin Taylor <s.justin.taylor@gmail.com>
2020-01-21 12:06:43 +1000

    added poco trade model

---
+++ src/GPIB.Models/ITrade.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace GPIB.Models {
+    public interface ITrade {
+        int ID { get; set; }
+        String Exchange { get; set; }
+        String ThirdPartyID { get; set; }
+        string Type { get; set; }
+        String Crypto { get; set; }
+        String Fiat { get; set; }
+        decimal CryptoAmount { get; set; }
+        decimal Fee { get; set; }
+        decimal GST { get; set; }
+        decimal rate { get; set; }
+        DateTime Created { get; set; }
+    }
+}
