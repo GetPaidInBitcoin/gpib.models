c37448914ab4d2ad7becb709cb28c0c6989fb9f0
Justin Taylor <s.justin.taylor@gmail.com>
2020-01-21 14:40:13 +1000

    changed from interface, added annotations

--- src/GPIB.Models/Trade.cs
+++ src/GPIB.Models/Trade.cs
@@ -1,17 +1,42 @@
 using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
-namespace GPIB.Models {
-    public interface ITrade {
+namespace GPIB.Models
+{
+    public class Trade
+    {
+        [Required]
         int ID { get; set; }
+
+        [Required]
         String Exchange { get; set; }
+
+        [Required]
         String ThirdPartyID { get; set; }
+
+        [Required]
         string Type { get; set; }
+
+        [Required]
         String Crypto { get; set; }
+
+        [Required]
         String Fiat { get; set; }
+
+        [Required]
         decimal CryptoAmount { get; set; }
+
+        [Required]
         decimal Fee { get; set; }
+
+        [Required]
         decimal GST { get; set; }
+
+        [Required]
         decimal Rate { get; set; }
+
+        [DatabaseGenerated (DatabaseGeneratedOption.Identity)]
         DateTime Created { get; set; }
     }
 }
