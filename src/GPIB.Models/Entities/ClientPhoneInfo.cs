2e46eb762ddf8cd71345bc4e0ccb6b6c71a6361c
Lucas Cullen <lucas@bitcoinbrisbane.com.au>
2020-05-16 20:03:01 +1000

    Auto stash before merge of "master" and "origin/master"

--- src/GPIB.Models/Entities/ClientPhoneInfo.cs
+++ src/GPIB.Models/Entities/ClientPhoneInfo.cs
@@ -5,10 +5,6 @@ namespace GPIB.Models
 {
     public class ClientPhoneInfo : BaseUserEntity, IEntity, IUserEntity
     {
-        public string PhoneNumber { get; set; }
-
-        public bool PhoneNumberConfirmed { get; set; }
-
         public PhoneStatus Status { get; set; }
 
         public string Code { get; set; }
