22ff4c6d27d168e28fc65612d7daf22278af0999
Alex <xiangzhe0812@gmail.com>
2020-05-17 07:37:24 +1000

    update (#13)
    * update
    
    * update

--- src/GPIB.Models/Entities/RapidID.cs
+++ src/GPIB.Models/Entities/RapidID.cs
@@ -6,7 +6,7 @@ namespace GPIB.Models
     public class RapidID
     {
         [DatabaseGenerated (DatabaseGeneratedOption.Identity)]
-        public Int32 Id { get; set; }
+        public Int32 ID { get; set; }
 
         public RapidIDRequestType RequestType { get; set; }
 
@@ -22,7 +22,7 @@ namespace GPIB.Models
 
         public Int32 RapidIDClientInfoId { get; set; }
 
-        [ForeignKey ("RapidIDClientInfoId")]
+        [ForeignKey ("RapidIDClientInfoID")]
         public RapidIDClientInfo RapidIDClientInfo { get; set; }
     }
 }
