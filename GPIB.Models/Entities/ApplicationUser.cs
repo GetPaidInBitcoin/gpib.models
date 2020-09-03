dfabf9aec00ec27abdd4d38f606fcb47d7362f80
Lucas Cullen <lucas@bitcoinbrisbane.com.au>
2020-09-03 19:11:58 +1000

    Refactor to guid

--- GPIB.Models/Entities/ApplicationUser.cs
+++ GPIB.Models/Entities/ApplicationUser.cs
@@ -6,7 +6,7 @@ namespace GPIB.Models
     public class ApplicationUser
     {
         [Required]
-        public String ID { get; set; }
+        public Guid ID { get; set; }
 
         public String Email { get; set; }
 
