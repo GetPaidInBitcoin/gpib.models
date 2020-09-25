bc6f095ff63f2b12148c6e4e7d5da665029771a7
Alex Xiang <xiangzhe0812@gmail.com>
2020-09-25 10:49:28 +1000

    added BaseUserEntity, IUserEntity

--- GPIB.Models/Entities/Settings.cs
+++ GPIB.Models/Entities/Settings.cs
@@ -3,7 +3,7 @@ using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 namespace GPIB.Models
 {
-    public class Settings
+    public class Settings : BaseUserEntity, IUserEntity
     {
         public Boolean Require2FAOnLogin { get; set; }
 
